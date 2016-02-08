4ff55ecf629ee8adf2188b0d46e2087e68aed839
SZibaee <sjianz01@mail.bbk.ac.uk>
2016-02-08 16:48:35 +0000

    most basic demo of using module exports

---
+++ node_js/modules/sadBag2js.js
@@ -0,0 +1,6 @@
+cureForSadness.fun();
+cureForSadness.happy();
+
+// Run this code by typing 'node sadBag2js.js'.
+// It throws an error because the 'happyFun.js' module has not been imported here (with require()).
+// The external function calls can be seen working in the other file 'sadBagjs.js' which does have the import.
---
+++ node_js/modules/sadBagjs.js
@@ -0,0 +1,4 @@
+var cureForSadness = require('./happyFun');
+cureForSadness.fun();
+cureForSadness.happy();
+// run this code by typing 'node sadBagjs.js in the terminal. 
